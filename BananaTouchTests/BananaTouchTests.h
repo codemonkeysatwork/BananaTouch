bbcc15e3e0f859f0ed150408a74642ae5ce5c8be
Edward Rudd <urkle@outoforder.cc>
2011-09-13 16:19:10 -0400

    Initial Commit

---
+++ BananaTouchTests/BananaTouchTests.h
@@ -0,0 +1,17 @@
+//
+//  BananaTouchTests.h
+//  BananaTouchTests
+//
+//  Created by Edward Rudd on 9/13/11.
+//  Copyright 2011 OutOfOrder.cc. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface BananaTouchTests : SenTestCase {
+@private
+    
+}
+
+@end
---
+++ BananaTouchTests/BananaTouchTests.m
@@ -0,0 +1,33 @@
+//
+//  BananaTouchTests.m
+//  BananaTouchTests
+//
+//  Created by Edward Rudd on 9/13/11.
+//  Copyright 2011 OutOfOrder.cc. All rights reserved.
+//
+
+#import "BananaTouchTests.h"
+
+
+@implementation BananaTouchTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in BananaTouchTests");
+}
+
+@end
