2897647a966a1f8570304fcc5b5e8dd09951b624
Edward Rudd <urkle@outoforder.cc>
2011-12-22 12:08:29 -0500

    pull in view decomposition code

---
+++ BananaTouch/BTViewUtils.h
@@ -0,0 +1,18 @@
+//
+//  BTViewUtils.h
+//
+//  Created by Edward Rudd on 1/2/09.
+//  Copyright 2009 OutOfOrder.cc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BTViewUtils : NSObject {
+
+}
+
++(void)decomposeView:(UIView *)view withIndent:(int)indent;
++(void)decomposeViewController:(UIViewController *)viewController;
++(UIView *)findSubViewByClassName:(NSString *)name UsingView: (UIView *)view;
+
+@end
---
+++ BananaTouch/BTViewUtils.m
@@ -0,0 +1,54 @@
+//
+//  BTViewUtils.m
+//
+//  Created by Edward Rudd on 1/2/09.
+//  Copyright 2009 OutOfOrder.cc. All rights reserved.
+//
+
+#import "BTViewUtils.h"
+
+@implementation BTViewUtils
+
++(void)decomposeView:(UIView *)view withIndent:(int)indent
+{
+#if defined(DEBUG)
+	NSString *nstr = [NSString stringWithFormat:@"%%-%ds- %%@  (T:%%x %%.0fx%%.0f+%%.0fx%%.0f)", indent];
+	NSLog(nstr, "", view, view.tag,
+				view.frame.size.width, view.frame.size.height,
+				view.frame.origin.x,view.frame.origin.y);
+	for (UIView *tView in view.subviews) {
+		[self decomposeView:tView withIndent:indent+2 ];
+	}
+#endif
+}
+
++(void)decomposeViewController:(UIViewController *)viewController
+{
+#if defined(DEBUG)
+	NSLog(@"Decomposing %@", viewController);
+	[self decomposeView: viewController.view withIndent:2];
+#endif
+}
+
++(UIView *)findSubViewByClassName:(NSString *)name UsingView: (UIView *)view
+{
+	@try {
+		NSString *temp = NSStringFromClass([view class]);
+//		NSLog(@"Checking %@ against %@", temp, name);
+		if ([name isEqualToString:temp]) {
+			return view;
+		}
+	}
+	@catch (NSException *exception) {
+		NSLog(@"Checking Short String against %@", name);
+	}
+	for (UIView *temp in view.subviews) {
+		temp = [self findSubViewByClassName:name UsingView:temp];
+		if (temp != nil) {
+			return temp;
+		}
+	}
+	return nil;
+}
+
+@end
